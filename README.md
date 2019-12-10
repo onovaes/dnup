42ff45a1d55b01371247848c4ba0cd7b353fb081
Gabriel Novaes <semproblema@gmail.com>
2019-12-10 10:11:40 -0400

    migration e readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# dnup
+# Shell Script para atualização de dothnews nas vm's
+
+Atualiza o SGI, o core, roda as migrations e testes unitários.
+
+wget -O - https://raw.githubusercontent.com/onovaes/dnup/master/dnup.sh | bash
+
--- dnup.sh
+++ dnup.sh
@@ -3,30 +3,30 @@
 #alias para git no cpanel
 alias git="/usr/local/cpanel/3rdparty/bin/git"
 
-printf "\n"
-
-
 # Atualizando o sgi
+printf "\n"
 cd ~/public_html/dothnews/
 echo 'Atualizando o SGI...'
 git pull
+
+#ATUALIZA O CORE
 printf "\n"
-
-
-
-#atualiza o core do dothnews
 echo 'Atualizando o core do dothnews...'
 cd ~/public_html/
 git pull
+
+#SETA PERMISSOES CORRETAS
 printf "\n"
-
-
-#SETA PERMISSAO CORRETA
 echo 'Permissão 644 em arquivos executaveis'
 chmod 644 ~/public_html/dothnews/index.php
 chmod 644 ~/public_html/image.php  
 chmod 644 ~/public_html/index.php
 printf "\n"
 
-#testes untiarios
+
+#MIGRATIONS
+php ~/public_html/index.php update migrate
+
+
+#TESTES UNTIARIOS
 php ~/public_html/index.php utest
