f84387ed736a4bbb8f01c697cf3cc1f287d02009
Gabriel Novaes <semproblema@gmail.com>
2020-06-02 11:37:15 -0400

    Mostrar a versao antes da atualizacao e garantir que o git esteja com o filemode desabilitado

--- dnup.sh
+++ dnup.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
+# Garantindo que o git esteja com fileMode false
+printf "\n"
+echo 'Set file mode no GIT'
+/usr/local/cpanel/3rdparty/bin/git config core.fileMode false
 
 # ATUALIZANDO O SGI
-printf "\n"
+printf "\n\n"
 cd ~/public_html/dothnews/
+echo 'Versao atual do SGI '
+/usr/local/cpanel/3rdparty/bin/git describe
+printf "\n"
 echo 'Atualizando o SGI...'
 /usr/local/cpanel/3rdparty/bin/git pull && /usr/local/cpanel/3rdparty/bin/git describe --always > version.txt
-
+printf "\n"
+echo 'Nova versao do SGI '
+cat version.txt
 
 # ATUALIZA O CORE
+printf "\n\n"
+cd ~/public_html/
+echo 'Versao atual do CORE DO NEWS '
+/usr/local/cpanel/3rdparty/bin/git describe
 printf "\n"
 echo 'Atualizando o core do dothnews...'
-cd ~/public_html/
 /usr/local/cpanel/3rdparty/bin/git pull && /usr/local/cpanel/3rdparty/bin/git describe > version.txt
+printf "\n"
+echo 'Nova versao do CORE do NEWS '
+cat version.txt
+
 
 
 #SETA PERMISSOES CORRETAS
-printf "\n"
+printf "\n\n"
 echo 'Permissão 644 em arquivos executaveis'
 chmod 644 ~/public_html/image.php  
 chmod 644 ~/public_html/index.php
 printf "\n"
 
 #SETA PERMISSOES CORRETAS
-printf "\n"
+printf "\n\n"
 echo 'Seta 755 to diretorios and 644 to files na pasta SGI'
 find ~/public_html/dothnews -type d -print0 | xargs -0 chmod 0755
 find ~/public_html/dothnews -type f -print0 | xargs -0 chmod 0644
@@ -39,4 +55,4 @@ php ~/public_html/dothnews/index.php update migrate
 php ~/public_html/index.php utest
 
 #TEMPORARIAMENTE - GOOGLE ANALYTICS MAI LIDAS
-php ~/public_html/index.php dnutils google mais_lidas
+#php ~/public_html/index.php dnutils google mais_lidas
