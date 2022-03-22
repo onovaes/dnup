3d160472c7933ec234863acab3046fe47fdf0de9
Gabriel Novaes <semproblema@gmail.com>
2022-03-22 17:29:58 -0400

    tira

--- cdnup.sh
+++ cdnup.sh
@@ -30,9 +30,7 @@ cat version.txt
 
 #CSV info files version.csv
 /usr/bin/git describe >> version.csv
-#echo ";" >> version.csv
 /usr/bin/git rev-parse --abbrev-ref HEAD >> version.csv
-#echo ";" >> version.csv
 
 
 #SETA PERMISSOES CORRETAS  image.php e index.php
